bf9238f151837ce7cfd10eb73707afaec80f2892
unknown <MadellynTC>
2025-04-27 21:53:20 -0500

    Calculadora

--- JavaApplication5/src/javaapplication5/JavaApplication5.java
+++ JavaApplication5/src/javaapplication5/JavaApplication5.java
@@ -1,20 +1,31 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+
 package javaapplication5;
 
-/**
- *
- * @author Usuario
- */
+import java.util.Scanner;
+
 public class JavaApplication5 {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Scanner numero = new Scanner (System.in);
+        System.out.println("Ingrese el primer numero:");
+        int num1 = numero.nextInt();
+        
+        System.out.println("Ingrese el segundo numero:");
+        int num2 = numero.nextInt();
+         
+        
+        int Suma = num1 + num2;
+        int Resta = num1-num2;
+        int Mult = num1*num2;
+        double Div = (double) num1/num2 ;
+        
+        System.out.println("Suma: " + Suma);
+        System.out.println("Resta: " + Resta);
+        System.out.println("Multiplicacion: " + Mult);
+        System.out.println("Division: " + Div);
     }
     
 }
